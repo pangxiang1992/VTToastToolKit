6e1005e073589a35b760be1c6108d8fc028ee552
庞翔 <pangxiangios@163.com>
2018-03-29 13:40:34 +0800

    modified demo

--- Demo/VTToastToolKitDemo/VTToastToolKitDemo/CartAlertView.swift
+++ Demo/VTToastToolKitDemo/VTToastToolKitDemo/CartAlertView.swift
@@ -29,3 +29,67 @@ class CartAlertView: UIView {
     }
     @IBOutlet weak var nameLable: UILabel!
 }
+// MARK: -
+extension UIView {
+    // XIB绑定的名字必须和类名字一样
+    class func loadFromXIB() -> UIView {
+        
+        let classNameCopy = NSStringFromClass(self.classForCoder())
+        let name = classNameCopy.components(separatedBy: ".").last
+        
+        let nibView = Bundle.main.loadNibNamed(name!, owner: self, options: nil)!.first
+        return nibView as! UIView
+    }
+    
+    var sdWidth: CGFloat {
+        get{
+            return self.frame.size.width
+        }
+        set{
+            var frame = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+    }
+    var sdHeight: CGFloat {
+        get{
+            return self.frame.size.height
+        }
+        set{
+            var frame = self.frame
+            frame.size.height = newValue
+            self.frame = frame
+        }
+    }
+    var sdX: CGFloat {
+        get{
+            return self.frame.origin.x
+        }
+        set{
+            var frame = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+    }
+    var sdY: CGFloat {
+        get{
+            return self.frame.origin.y
+        }
+        set{
+            var frame = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+    }
+    var sdSize: CGSize {
+        get{
+            return self.frame.size
+        }
+        set{
+            var frame = self.frame
+            frame.size = newValue
+            self.frame = frame
+        }
+    }
+    
+}
